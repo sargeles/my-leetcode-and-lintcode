97d76be41a2c75e415a6afa7fa8389f58d4df0d0
sargeles <330657387@qq.com>
2016-11-08 10:28:40 +0800

    添加一个问题。递归的

---
+++ src/algorithm/InvertTree.java
@@ -0,0 +1,37 @@
+package algorithm;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
+public class InvertTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+	}
+}
+/*
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+		if (root == null)
+			return root;
+		TreeNode temp;
+		temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+		invertTree(root.left);
+		invertTree(root.right);
+		return root;
+	}
+}*/
